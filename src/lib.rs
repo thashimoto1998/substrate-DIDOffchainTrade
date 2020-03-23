8ce77be2805176198a1a34d391bff549ef91b51e
Tatsuki Hashimoto <hashimoto19980924@gmail.com>
2020-03-23 20:46:07 +0900

    master

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@
 
 use codec::{Decode, Encode};
 use frame_support::{
-	decl_module, decl_storage, decl_event, decl_error, dispatch
+	decl_module, decl_storage, decl_event, decl_error, 
+	dispatch::DispatchResult, ensure, StorageMap,
 };
+use sp_runtime::traits::{Hash, IdentifyAccount, Member, Verify};
 use sp_std::{prelude::*m vec::Vec};
 use system::ensure_signed;
-use sp 
+use sp_core::RuntimeDebug; 
 
 #[cfg(test)]
 mod mock;
@@ -14,87 +16,106 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Default, RuntimeDebug)]
+pub struct AccessCondition<AccountId> {
+	pub nonce: u32,
+	pub players: Vec<AccountId>,
+	pub seqNum: u32,
+	pub status: AppStatus,
+	pub owner: AccountId,
+	pub grantee: AccountId,
+	pub did: AccountId,
+	pub didList: Vec<AccountId>,
+	pub documentPermissionsState: map (AccountId, AccountId) => bool;
+	pub const key: u32,
+}
+
+pub enum AppStatus {
+	IDLE,
+	FINALIZED,
+}
+
+type AcessConditionOf<T> = AccessCondition<<T as system::Trait>::AccountId>;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Default, RuntimeDebug)]
+pub struct AppState<AccountId> {
+	pub nonce: u32,
+	pub seqNum: u32,
+	pub state: AccountId,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Default, Runtime)]
+pub struct StateProof<AccountId, Signature> {
+	pub appState: AppState<AccountId>,
+	pub sigs: Vec<Signature>,
+}
+
 /// The pallet's configuration trait.
-pub trait Trait: system::Trait {
-	// Add other types and constants required to configure this pallet.
-
-	/// The overarching event type.
+pub trait Trait: system::Trait + timestamp::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+	type Public: IdentifyAccount<AccountId = Self::AccountId>;
+	type Signature: Verify<Signer = Self::Public> + Member + Decode + Encode;
 }
 
-// This pallet's storage items.
+
 decl_storage! {
-	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(fn something): Option<u32>;
+	trait Store for Module<T: Trait> as DIDOffchainTrade {
+		pub AccessConditionOf get(appinfo_of): 
+			map hasher(blake2_256) T::AccountId => AccessConditionOf<T>;
+		pub keyOf get(key_of): map u32 => T::AccountId;
+		pub FinalizedOf: map hasher(blake2_256) T::AccountId => bool;
+		pub OutcomeOf: map hasher(blake2_256) T::AccountId => bool;
 	}
 }
 
-// The pallet's events
-decl_event!(
-	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		/// Just a dummy event.
-		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		/// To emit this event, we call the deposit function, from our runtime functions
-		SomethingStored(u32, AccountId),
-	}
-);
-
-// The pallet's errors
-decl_error! {
-	pub enum Error for Module<T: Trait> {
-		/// Value was None
-		NoneValue,
-		/// Value reached maximum and cannot be incremented further
-		StorageOverflow,
-	}
-}
-
-// The pallet's dispatchable functions.
 decl_module! {
-	/// The module declaration.
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		// Initializing errors
-		// this includes information about your errors in the node's metadata.
-		// it is needed only if you are using errors in your pallet
 		type Error = Error<T>;
 
-		// Initializing events
-		// this is needed only if you are using events in your pallet
 		fn deposit_event() = default;
 
-		/// Just a dummy entry point.
-		/// function that can be called by the external world as an extrinsics call
-		/// takes a parameter of the type `AccountId`, stores it, and emits an event
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let who = ensure_signed(origin)?;
+		pub fn createAccessCondition() {}
 
-			// Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			Something::put(something);
+		pub fn intendSettle() {}
 
-			// Here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			Ok(())
-		}
+		pub fn getStatus() {}
 
-		/// Another dummy entry point.
-		/// takes no parameters, attempts to increment storage value, and possibly throws an error
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let _who = ensure_signed(origin)?;
+		pub fn getSeqNum() {}
 
-			match Something::get() {
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					Something::put(new);
-					Ok(())
-				},
-			}
-		}
+		pub fn isFinalized() {}
+
+		pub fn getOutcome() {}
+
+		pub fn checkPermissions() {}
+
+		pub fn setNewDID() {}
 	}
+}
+
+decl_event!(
+	pub enum Event<T>
+	where
+	<T as system::Trait>::AccountId,
+	<T as system::Trait>::BlockNumber,
+	{
+		AccessConditionCreated(AccountId, AccountId, AccountId, u32),
+		IntendSettle(u32, BlockNumber),
+		IDLE_STATUS(AccountId, BlockNumber),
+		FINALIZED_STATUS(AccountId, BlockNumber),
+		BooleanOutcome(bool),
+		AccessPermission(bool),
+		NewDID(AccountId),
+	}
+)
+
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+		NotOwner,
+		InvalidSeqNum,
+		InvalidSignature,
+	}
+}
+
+impl<T: Trait> Module<T> {
+	pub fn verifySignature() {}
 }
