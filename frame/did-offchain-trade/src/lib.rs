7e82d66375ef0874d8f2e1b6922b10e512d36687
Tatsuki Hashimoto <hashimoto19980924@gmail.com>
2020-03-25 19:36:50 +0900

    feature/325

--- frame/did-offchain-trade/src/lib.rs
+++ frame/did-offchain-trade/src/lib.rs
@@ -18,6 +18,13 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"offchain-trade");
+pub mod crypto {
+	use super::KEY_TYPE;
+	use sp_runtime::app_crypto::{app_crypto, sr25519};
+	app_crypto!(sr255199, KEY_TYPE);
+}
+
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Default, RuntimeDebug)]
 pub struct AccessCondition<AccountId> {
 	pub nonce: u32,
@@ -27,7 +34,6 @@ pub struct AccessCondition<AccountId> {
 	pub owner: AccountId,
 	pub grantee: AccountId,
 	pub did: AccountId,
-	pub didList: Vec<AccountId>,
 	pub documentPermissionsState: map (AccountId, AccountId) => bool;
 	pub key: u32,
 }
@@ -41,17 +47,18 @@ pub enum AppStatus {
 type AcessConditionOf<T> = AccessCondition<<T as system::Trait>::AccountId>;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Default, RuntimeDebug)]
-pub struct AppState<AccountId> {
+pub struct AppState {
 	pub nonce: u32,
 	pub seqNum: u32,
-	pub state: AccountId,
+	pub state: u32,
 }
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Default, RuntimeDebug)]
-pub struct StateProof<AccountId, Signature> {
-	pub appState: AppState<AccountId>,
+pub struct StateProof<Signature> {
+	pub appState: AppState,
 	pub sigs: Vec<Signature>,
 }
+
 
 /// The pallet's configuration trait.
 pub trait Trait: system::Trait + timestamp::Trait {
@@ -59,16 +66,21 @@ pub trait Trait: system::Trait + timestamp::Trait {
 	type Public: IdentifyAccount<AccountId = Self::AccountId>;
 	type Signature: Verify<Signer = Self::Public> + Member + Decode + Encode;
 	type DIDOwner: DIDOwner<AccountId = Self::AccountId>;
+	type SubmitSignedTransaction: offchain::SubmitSignedTransaction<Self, <Self as Trait>::Call>;
+	type Call: From<Call<Self>>;
+	type GracePeriod: Get<Self::BlockNumber>;
 }
 
 
 decl_storage! {
 	trait Store for Module<T: Trait> as DIDOffchainTrade {
-		pub Key get(key): u32;
+		pub AccessConditionAddressKey get(fn key): u32;
+		pub AccessConditionAddressList get(fn key_of): map u32 => T::AccountId;
 		pub AccessConditionList get(condition_list): 
 			map hasher(blake2_256) T::AccountId => AccessConditionOf<T>;
-		pub KeyOf get(key_of): map u32 => T::AccountId;
-		pub DocumentPermissionsState get(permission):
+		pub DIDKey get(fn did_key): u32;
+		pub DIDList get(fn did_list): map u32 => T::AccountId;
+		pub DocumentPermissionsState get(fn permission):
 			double_map hasher(blake2_256) T::AccountId, hasher(blake2_256) T::AccountId => bool;
 		pub FinalizedOf: map hasher(blake2_256) T::AccountId => bool;
 		pub OutcomeOf: map hasher(blake2_256) T::AccountId => bool;
@@ -89,19 +101,33 @@ decl_module! {
 		) -> DispatchResult {
 			let _ = ensure_signed(origin)?;
 
+			//TODO: Refactoring
 			// Create new Address of Access Condition
-			let key: u32 = <Key>::get();
-			let key_string: String = key.to_string();
+			let _key: u32 = <AccessConditionAddressKey>::get();
+			let key_string: String = _key.to_string();
 			let access_condition_string: String = "AccessCondition" + key_string;
 			let access_condition_pair: sr25519::Pair = Self::account_pair(access_condition_string);
 			let access_condition_public: sr25519::Public = Self::account_key(access_condition_pair);
+
+			// TODO: Refactoring and default <DIDKey> is 2.
+			let mut didKey: u32 = |_didKey| {
+				let mut _didKey = Self::did_key();
+				if (_didKey == 0 || _didKey == 1) {
+					_didKey = 2;
+					_didKey
+				} else {
+					_didKey
+				}
+			}
+			<DIDList<T>>::insert(didKey, &did);
+			<DIDKey>::mutate(|key| *key += 1);
 
 			let isPlayer1: bool = T::DIDOwner::is_did_owner(&did, &players[0]);
 			let isPlayer2: bool = T::DIDOwner::is_did_owner(&did, &players[1]);
 			ensure!(isPlayer1 == true || isPlayer2 == true, Error::<T>::NotOwner);
 			
+			// TODO: Refactoring
 			if (isPlayer1 == true) {
-				let _didList: Vec<T::AccountId> = vec![did];
 				let access_condition = AccessConditionOf<T> {
 					nonce: nonce,
 					players: players.clone(),
@@ -110,25 +136,25 @@ decl_module! {
 					owner: players[0].clone(),
 					grantee: players[1].clone(),
 					did: did,
-					didList: _didList,
-					key: key,
+					key: _key,
 				};
-				<AccessConditionList<T>>::insert(access_condition_public.clone(), access_condition);
-				<KeyOf<T>>::insert(key, access_condition_public.clone());
+
+				<AccessConditionAddressKey>::mutate(|key| *key += 1);
+				<AccessConditionAddressList<T>>::insert(_key, access_condition_public.clone());
+				<AccessConditionAddressList<T>>::insert(access_condition_public.clone(), access_condition.clone());
+				<KeyOf<T>>::insert(_key, access_condition_public.clone());
 				<DocumentPermissionsState<T>>::insert(&did, players[1].clone(), false);
 				<FinalizedOf<T>>::insert(access_condition_public.clone(), false);
 				<OutcomeOf<T>>::insert(access_condition_public.clone(), false);
-				let _key = key + 1;
-				<Key>::put(_key);
+				
 				Self::deposit_event(RawEvent::AccessConditionCreated(
 					access_condition_public,
 					players[0],
 					players[1],
-					key,
+					_key,
 				));
 				Ok(())
 			} else {
-				let _didList: Vec<T::AccountId> = vec![did];
 				let access_condition = AccessConditionOf<T> {
 					nonce: nonce,
 					players: players.clone(),
@@ -137,27 +163,43 @@ decl_module! {
 					owner: players[1],
 					grantee: players[0],
 					did: did,
-					didList: _didList,
-					key: key,
+					key: _key,
 				};
+
+				<AccessConditionAddressKey>::mutate(|key| *key += 1);
+				<AccessConditionAddressList<T>>::insert(_key, access_condition_public.clone());
 				<AccessConditionList<T>>::insert(access_condition_public.clone(), access_condition);
-				<KeyOf<T>>::insert(key, access_condition_public.clone());
+				<KeyOf<T>>::insert(_key, access_condition_public.clone());
 				<DocumentPermissionsState<T>>::insert(&did, players[0].clone(), false);
 				<FinalizedOf<T>>::insert(access_condition_public.clone(), false);
 				<OutcomeOf<T>>::insert(access_condition_public.clone(), false);
-				let _key = key + 1;
-				<Key>::put(_key);
+
 				Self::deposit_event(RawEvent::AccessConditionCreated(
 					access_condition_public,
 					players[1],
 					players[0],
-					key
+					_key
 				));
 			}
 
 		}
 
-		pub fn intendSettle() {}
+		pub fn intendSettle(
+			origin, 
+			condition_address: T::AccountId,
+			transaction: StateProof<T::Signature>,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			let players: Vec<T::AccountId> = Self::condition_list(&condition_address).players;
+			ensure!(&who == &players[0] || &who == &palyers[1], "sender is not players");
+			
+			let mut encoded = transaction.appState.nonce.encode();
+			encoded.extend(transaction.appState.seqNum.encode());
+			encoded.extend(transaction.appState.state.encode());
+
+			Self::valid_signers(transaction.sigs, encoded, players);
+			
+		}
 
 		pub fn getStatus(origin, condition_address: T::AccountId) -> DispatchResult {
 			let _ = ensure_signed(origin)?;
@@ -258,7 +300,21 @@ decl_error! {
 }
 
 impl<T: Trait> Module<T> {
-	pub fn verifySignature() {}
+	pub fn valid_signers(
+		signatures: Vec<T::Signature>,
+		msg: &[u8],
+		signers: Vec<T::AccountId>,
+	) -> DispatchResult {
+		if ((&signatures[0].verify(msg, &signers[0])) && (&signatures[1].verify(msg, &signers[1]))
+			|| ((&signatures[1].verify(msg, &signers[1]) && (&signatures[0].verify(msg, &signers[0]))))
+		) {
+			Ok(())
+		} else {
+			Err(Error::<T>::InvalidSignature.into())
+		}
+	}
+
+
 
 	pub fn account_pair(s: &str) -> sr25519::Pair {
 		sr25519::Pair::from_string(&format!("//{}", s), None)
